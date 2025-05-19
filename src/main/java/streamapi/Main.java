9d799310b6f9ff67ae207e96effdea84f61e273e
Colin Meihöfer <colin.meihoefer@gmx.de>
2025-05-19 12:57:22 +0200

    Use streamAPI to get imf student cps

--- src/main/java/streamapi/Main.java
+++ src/main/java/streamapi/Main.java
@@ -2,6 +2,7 @@ package streamapi;
 
 import java.io.InputStream;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /** Starter for the stream api task. */
 public class Main {
@@ -52,17 +53,17 @@ public class Main {
      * @return Set of credit points of all IFM students
      */
     public static Set<Integer> ifmCps(List<Student> studentList) {
-        // TODO
-        Set<Integer> result = new HashSet<>();
-        Integer i = 0;
-        for (Student v : studentList) {
-            if (v.isIFM()) {
-                i = v.cps();
-                result.add(i);
-            }
-        }
 
-        return result;
+        // ein set / menge ist eine collection von elementen, wo bei jedes element nur ein mal vorkommt
+        // diese methode gibt zurück, welche verschiedenen cps die studenten haben
+        // studentList.Where(x => x.isFIM).Select(x => x.cps).ToHashSet()
+
+        return studentList.stream()
+//            .filter(x -> x.isIFM())
+//            .map(x -> x.cps())
+            .filter(Student::isIFM)
+            .map(Student::cps)
+            .collect(Collectors.toSet());
     }
 
     /**
